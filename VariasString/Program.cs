d90c72a6f2cf7a2eca833e9c7f9597a65fa1d7d3
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-08 10:18:45 +0000

    EX02

--- VariasString/Program.cs
+++ VariasString/Program.cs
@@ -7,11 +7,26 @@ namespace VariasString
     {
         static void Main(string[] args)
         {
+            //EX01//
             string s = "ola \u058D";
             Console.WriteLine(s);
 
             string t = "C:\\Users\\Vitor\\Documents\\OneDrive";
             Console.WriteLine(t);
+
+            //EX02//
+            string r = "batata\t" + "mó" + "!!!";
+            r += "\tbem bom";
+            Console.WriteLine(r);
+
+            int x = 34;
+            int y = 35;
+            string i = $"{x} mais {y} é igual a {x + y}";
+            Console.WriteLine(i);
+
+            string n = string.Format("batata mó!!! é igual a {0}", x+y);
+            Console.WriteLine(n);
+
 
             Console.ReadKey();
         }
