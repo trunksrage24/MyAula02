18b77d90fdd45b839bd1c4890b5c95d2486fba87
Vitor Daniel <vitor.daniel@outlook.pt>
2019-03-08 09:20:26 +0000

    Add project files.

---
+++ VariasString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariasString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
